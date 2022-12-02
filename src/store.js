93ba7266e5ca6c72df1836ba36db8651edbfbf5a
netapy <39877685+netapy@users.noreply.github.com>
2022-12-02 18:26:59 +0100

    editing rating

--- src/store.js
+++ src/store.js
@@ -1,8 +1,21 @@
-import { writable } from 'svelte/store';
-import './appwrite.js';
+import { writable } from "svelte/store";
+import "./appwrite.js";
 
 const sdk = require("node-appwrite");
 export const client = new sdk.Client();
 export const users = new sdk.Users(client);
 export const databases = new sdk.Databases(client);
 export const query = sdk.Query;
+
+export const getUserName = async (user_id) => {
+  let query = databases.getDocument("633fe0ac171c362df477", "633fe0baf186f65a0376", user_id);
+  query.then(
+    (r) => {
+        return "agaegae";
+    },
+    (e) => {
+        return "error";
+    }
+  );
+  
+};
