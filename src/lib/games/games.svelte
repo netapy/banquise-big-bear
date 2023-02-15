7e34351abf7ea1d585d3aa229d8b156364056b59
Baudouin A <39877685+netapy@users.noreply.github.com>
2023-02-15 09:57:06 +0100

    display all games

--- src/lib/games/games.svelte
+++ src/lib/games/games.svelte
@@ -1,6 +1,5 @@
 <script>
   import { Grid } from "gridjs";
-  import "gridjs/dist/theme/mermaid.css";
   import { onMount } from "svelte";
   import "../../appwrite.js";
   import { databases, query } from "../../store.js";
@@ -13,21 +12,54 @@
   let idMap = {};
   let selectedId = "";
 
+  const gettingAllGames = async () => {
+    let documents = [];
+    let query_param_games = [
+      query.limit(100),
+      query.orderDesc(""),
+      query.limit(100),
+    ];
+    while (true) {
+      let query_result = await databases.listDocuments(
+        "62e0fd281976e7171db9",
+        "62e0fe08a4a5d6592df2",
+        query_param_games
+      );
+      documents = documents.concat(query_result["documents"]);
+      if (query_result["documents"].length == 0) {
+        break;
+      }
+      query_param_games[2] = query.cursorAfter(
+        query_result["documents"][query_result["documents"].length - 1].$id
+      );
+    }
+    return documents;
+  };
+
   const buildTable = async () => {
-    const gamesDataReq = await databases.listDocuments("62e0fd281976e7171db9", "62e0fe08a4a5d6592df2", [query.limit(100), query.orderDesc("$updatedAt")]);
-    idMap = await getUserNames(gamesDataReq.documents.map((x) => x["owner"]));
-    gamesData = gamesDataReq["documents"];
+    const gamesDataReq = await gettingAllGames();
+    //  idMap = await getUserNames(gamesDataReq.map((x) => x["owner"]));
+    gamesData = gamesDataReq;
     grid = new Grid({
       autoWidth: true,
       resizable: true,
       search: true,
       sort: true,
-      style: { table: { "font-size": "12px" } },
-      columns: ["$id", "Title", "Owner", "$createdAt", "category", "Public", "nsfw"],
+      style: { table: { "font-size": "14px" },  },
+      columns: [
+        "$id",
+        "Title",
+        "Owner",
+        "$createdAt",
+        "category",
+        "Public",
+        "nsfw",
+      ],
       data: gamesData.map((game) => [
         game.$id,
         game["title"],
-        idMap[game["owner"]],
+        game["owner"],
+        //  idMap[game["owner"]],
         game["$createdAt"].split("T")[0],
         game["category"].join(", "),
         String(game["public"]),
@@ -44,15 +76,17 @@
   const filterPublicGames = (e) => {
     if (e.target.checked) {
       grid.updateConfig({
-        data: gamesData.filter(x=>x["public"] != false).map((game) => [
-          game.$id,
-          game["title"],
-          idMap[game["owner"]],
-          game["$createdAt"].split("T")[0],
-          game["category"].join(", "),
-          String(game["public"]),
-          String(game["nsfw"]),
-        ]),
+        data: gamesData
+          .filter((x) => x["public"] != false)
+          .map((game) => [
+            game.$id,
+            game["title"],
+            idMap[game["owner"]],
+            game["$createdAt"].split("T")[0],
+            game["category"].join(", "),
+            String(game["public"]),
+            String(game["nsfw"]),
+          ]),
       });
     } else {
       grid.updateConfig({
@@ -75,14 +109,20 @@
   });
 
   async function getUserNames(userIds) {
-    const result = await databases.listDocuments("633fe0ac171c362df477", "633fe0baf186f65a0376", [query.equal("$id", userIds)]);
+    const result = await databases.listDocuments(
+      "633fe0ac171c362df477",
+      "633fe0baf186f65a0376",
+      [query.equal("$id", userIds)]
+    );
     const idMap = {};
     for (const theid of userIds) {
-      idMap[theid] = result.documents.some((el) => el.$id == theid) ? result.documents.find((el) => el.$id === theid)["user_name"] : theid;
+      idMap[theid] = result.documents.some((el) => el.$id == theid)
+        ? result.documents.find((el) => el.$id === theid)["user_name"]
+        : theid;
     }
     return idMap;
   }
-  
+
   function handleClick(id) {
     openModal(Modal, { gameId: id });
   }
@@ -90,19 +130,31 @@
 
 <div class="p-3 mb-3">
   <h3>Key data</h3>
-  <div>{gamesData.length} games | {gamesData.filter((x) => x["public"] == true).length} posted games</div>
+  <div>
+    {gamesData.length} games | {gamesData.filter((x) => x["public"] == true)
+      .length} posted games
+  </div>
 </div>
 <div class="p-3">
   <h3>All games</h3>
   <div class="p-3">
-    <input type="checkbox" class="me-2" id="publicIO" on:change={filterPublicGames} /><label for="publicIO"
-      >Only public games</label
-    >
+    <input
+      type="checkbox"
+      class="me-2"
+      id="publicIO"
+      on:change={filterPublicGames}
+    /><label for="publicIO">Only public games</label>
   </div>
   <div id="tableWrapper" />
 </div>
 <Modals>
-  <div slot="backdrop" class="backdrop" on:click={closeModal} on:keydown={closeModal} transition:fade />
+  <div
+    slot="backdrop"
+    class="backdrop"
+    on:click={closeModal}
+    on:keydown={closeModal}
+    transition:fade
+  />
 </Modals>
 
 <style>
@@ -114,4 +166,5 @@
     left: 0;
     background: rgba(0, 0, 0, 0.5);
   }
+
 </style>
